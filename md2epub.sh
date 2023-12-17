718e6955021aa6c1740662f8ec33df74694a9d1c
Florian Schendel <mail@florianschendel.de>
2023-12-17 20:03:44 +0100

    bugfix title

--- md2epub.sh
+++ md2epub.sh
@@ -2,8 +2,7 @@
 
 while getopts d: flag
 do
-    case "${flag}" in
-        d) md_directory=${OPTARG};;
+    case "${flag}" in d) md_directory=${OPTARG};;
     esac
 done
 echo "Converting in directory: $md_directory";
@@ -16,10 +15,10 @@ temp=filelist.cache
 find "$md_directory" -name '*.md' -maxdepth 1 > $temp
 
 while read file; do
-    title=`echo ${file%.md}`
+    titleName=`echo ${file%.md}`
     file=`echo ${file%.md}`
-    IFS='/' array=($title)
-    #pandoc --from markdown --to epub3 "$file.md" --output "$file.epub" --toc
-    pandoc --from markdown --to epub3 "$file.md" --output "$file.epub" --toc --metadata title="${array[${#array[@]}-1]}"
+    basename "$titleName"
+    title="$(basename -- $titleName)"
+    pandoc --from markdown --to epub3 "$file.md" --output "$file.epub" --toc=true --metadata title=$title
 done <  $temp
 #rm $temp
